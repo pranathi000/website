dc8e406c3917a2d94b5c0e3a37a6912e39acd632
santhoshini <89771081+pranathi000@users.noreply.github.com>
2022-11-28 19:09:12 +0530

    Delete pro_final directory

--- pro_final/css/anime.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
-
-menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("active");
-    navigation.classList.toggle("active");
-});
